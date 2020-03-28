e040b39d3df0afb98d681993919ef3ae774e1075
Lucas Paulo <mesakilive@gmail.com>
2020-03-28 14:18:50 -0300

    Be The Hero

---
+++ backend/anotation.js
@@ -0,0 +1,36 @@
+/* 
+    * Rota e Recurso
+*/
+
+/*
+* Métodos HTTP
+*
+*   GET: buscar uma informação do back-end
+*   POST: criar uma informação no back-end
+*   PUT: Alterar uma informação no back-end
+*   DELETE: Deletar uma informação no back-end
+*
+*/
+
+ /*
+ * Tipos de Parâmetros:
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request body: O corpo da requisição, utilizado para criar e alterar recursos.
+ * 
+ * Executar o servidor nodemon -> npm start
+ * Executar um pacote do knex -> npx knex init
+ * 
+ */
+
+
+/*
+* SQL: Mysql, SQLite, PostGreSQL, Oracle, Microsoft SQL Server
+* NoSql: MongoDb, CouchDb, etc  
+*/
+
+/*  
+*   Driver: SELECT * FROM users;
+*   Query Builder: table('users').select('*').where()....
+*/
