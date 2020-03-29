bb478d9457ccec2ee2cc7041c4486f63862e5e55
Lucas Paulo <mesakilive@gmail.com>
2020-03-29 01:04:52 -0300

    Finalizing the project

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,9 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+// Verifica se está em ambiente de teste ou produção
+const config = process.env.NODE_ENV == 'test' ? configuration.test : configuration.development;
+
+const connection = knex(config);
 
 module.exports = connection;
